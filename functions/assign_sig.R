be91fda79882c6a10f07b5c9dd08568dc8de346c
Debra Duarte <debra.duarte@noaa.gov>
2025-09-15 14:07:09 -0400

    add permanova

--- functions/assign_sig.R
+++ functions/assign_sig.R
@@ -5,6 +5,7 @@ assign_sig <- function(dat) {
            dAIC_sig = ifelse(glmmconv==1, ifelse(AICd > 2, 1, 0), NA),
            KS_test_sig = ifelse(KSp < 0.05, 1, 0),
            median_test_sig = ifelse(ci_lo > 0 | ci_hi < 0, 1, 0),
-           mvglm_sig = ifelse(mvglm_p < 0.05, 1, 0)
+           mvglm_sig = ifelse(mvglm_p < 0.05, 1, 0),
+           perma_sig = ifelse(perma_p < 0.05, 1, 0)
     ) 
 }
