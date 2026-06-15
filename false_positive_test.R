5f26143e68c48e8c756287bf273d9e3391109a07
CraigFaunce-NOAA <craig.faunce@noaa.gov>
2026-06-15 18:47:11 +0000

    Updated packages to load afsc packages.

--- false_positive_test.R
+++ false_positive_test.R
@@ -1,7 +1,7 @@
 # Packages and Functions ----
 library(tidyverse)
-library(gdrive)       # devtools::install_github("noaa-afsc/gdrive")
-library(mvobservr)    # devtools::install_github("noaa-afsc/mvobservr")
+library(gdrive)     # pak::pak("noaa-afsc/gdrive")
+library(mvobservr)  # pak::pak("noaa-afsc/mvobservr") instead.
 library(mgcv)         # for gam
 library(lme4)
 library(tweedie)
